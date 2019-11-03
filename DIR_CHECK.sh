ca99d2a9d25e94369549ea270e4071361f1fa09b
reg3x <marcinr92@outlook.com>
2019-11-03 14:04:08 +0100

    CHENGES IN DIR_CHECK

--- DIR_CHECK.sh
+++ DIR_CHECK.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 OK="[\033[32mOK\e[0m]"
-WAIT="[\033[32m*\e[0m]"
 CHECKDIR="[\033[32mCHECK\e[0m]"
-WAR="[\033[31m!\e[0m]"
+WAR="[\033[31mWARNING!\e[0m]"
 INFO="[\033[34mINFO\e[0m]"
 INFOW="[\033[31mINFO\e[0m]"
 
@@ -19,28 +18,50 @@ echo -e $1 $2
 }
 
 CHECKSUM(){
-echo $(du -sh $1 2>/dev/null |awk '{print $1}' | sha1sum | awk '{print $1}') $1 >> log          
+echo $(du -sh $1 2>/dev/null |awk '{print $1}' | sha1sum | awk '{print $1}') $1 > $(date +"%m-%d-%y")
 }
 
+CHECK_DIR(){
+    DIR="tmp"
+    
+    local TODAY=$(date "+%m-%d-%y")
+    local YESTERDAY=$(date -d "1 day ago" '+%m-%d-%y')
+    
+    
+    if [ -d "/$DIR" ];then
+    
+        HEADER $INFO "CHECK /$DIR DIR" 	
+    
+        if [ -e "log/$TODAY" ];then
+    
+            DEBUG $CHECKDIR "CHECK LOG FILE: $DATE"
+            # echo $(du -sh /tmp 2>/dev/null |awk '{print $1}' | sha1sum | awk '{print $1}') $1 > ./log/"$(date +'%m-%d-%y')" 
 
-HEADER $INFO "CHECK TMP DIR" 	
-if [ -e "/$1" ];then
-DEBUG $INFO  "$1 ALREADY EXIST"
-DEBUG $CHECKDIR "CHECK /$1 DIR"
-if [ -e "log" ];then
-	DEBUG $WAIT "VERIFIES CHECKSUM"
-	DIFF=$(diff -q log log1)
-	if [ "$DIFF" != "" ];then
-		DEBUG $INFOW "INVALID CHECKSUM"
-	else
-		DEBUG $OK "CHECKSUM VALID"
-	fi
-
-else
-	DEBUG $INFO "CREATE CHECKSUM"
-	CHECKSUM "/tmp"
-	# echo $(du -sh /tmp 2>/dev/null |awk '{print $1}' | sha1sum | awk '{print $1}') $1 >> log 
-fi
-else
-DEBUG $WAIT "DIR DOESN'T EXIST"
-fi
+            if [ -e "log/$YESTERDAY" ];then
+                DEBUG $CHECKDIR "COMPARE LOGS: from $TODAY to $YESTERDAY"
+                DIFF=$(diff -q log/$TODAY log/$YESTERDAY)
+            
+                if [ "$DIFF" != "" ];then
+                
+                    DEBUG "$WAR" "THE FILE HAS BEEN MODIFIED"
+                    DEBUG "$INFOW" "LAST MODIFIED"
+                else
+                    DEBUG "$OK" "FILE OK"
+                
+                fi
+            else
+                DEBUG "$INFOW" "NOTHING TO COMPARE"
+            fi
+        
+        else
+    
+            DEBUG $INFO "CREATE LOG FILE FOR: /tmp"
+            echo $(du -sh /tmp 2>/dev/null |awk '{print $1}' | sha1sum | awk '{print $1}') $1 > log/"$TODAY"       
+        
+        fi
+    else
+        
+        DEBUG "$INFOW" "FILE DOESN'T EXIST"
+    fi
+}
+CHECK_DIR
--- NETWORK_CHECK.sh
+++ NETWORK_CHECK.sh
@@ -25,8 +25,7 @@ CHECKSUM(){
 echo $(du -sh $1 2>/dev/null |awk '{print $1}' | sha1sum | awk '{print $1}') $1 >> log          
 }
 
-
-
+LOCAL_MACHINE_IP=""
 LOOP_BACK="127.0.0.1"
 LOCAL_LAN="192.168.1.0"
 I_ETH="eth0"
@@ -37,20 +36,24 @@ I_WLAN_mon="wlan0man"
 # PORTS
 
 P_FTP="21"
+P_SSH="22"
+P_SMTP="25"
 P_HTTP="80"
 P_POP="110"
 P_SQL="3306"
-P_SSH="22"
-P_SMTP="25"
-
-# ip netconf
-# ip route
 
 HEADER $INFO "NETWORK DETAILS"
 
 get_local_ip(){
-    ip=$(python3 pyScript/ip.py)
-    echo -e "$LOCALIP $ip"    
+
+    local ip=$(python3 script/ip.py)
+    LOCAL_MACHINE_IP=$ip
+    
+    if [ "$ip" != "" ];then
+        DEBUG "$LOCALIP $ip"
+    else
+        DEBUG  "$INFOW NO CONNECTED"
+    fi
 }
 
 get_interface(){
@@ -68,15 +71,22 @@ get_interface(){
 
 
 find_host(){
-    DEBUG "$INFOW" "CHECK LOCAL NETWORK: $LOCAL_LAN"
-    nmap -n -sn "$LOCAL_LAN/24" -oX report/xml_raport.xml 1>/dev/null
-    ACTIVE_HOSTS=($(python3 pyScript/xml_parser.py))
-    element_count=${#ACTIVE_HOSTS[@]}
     
-    for i in $(seq 0 $(expr $element_count - 1));do
+    if [ "$LOCAL_MACHINE_IP" != "" ];then
+        
+        DEBUG "$INFOW" "CHECK LOCAL NETWORK: $LOCAL_LAN"
+        
+        nmap -n -sn "$LOCAL_LAN/24" -oX report/xml_raport.xml 1>/dev/null
+        ACTIVE_HOSTS=($(python3 script/xml_parser.py))
+        element_count=${#ACTIVE_HOSTS[@]}
     
-        DEBUG "$ACTIVE_HOST ${ACTIVE_HOSTS[$i]}"
-    done
+        for i in $(seq 0 $(expr $element_count - 1));do
+            DEBUG "$ACTIVE_HOST ${ACTIVE_HOSTS[$i]}"
+        done
+        
+    else
+            DEBUG "$INFOW" "No ip detected. Can't start scanning"
+    fi
 }
 
 
