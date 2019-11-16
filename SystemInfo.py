5d96fd6d232fe2f26171b613665e00c446879188
reg3x <marcinr92@outlook.com>
2019-11-16 16:17:25 +0100

    modified sysinfo class

--- SystemInfo.py
+++ SystemInfo.py
@@ -1,16 +1,15 @@
 import subprocess
 from subprocess import Popen, PIPE
-import os
 import time
 import os
-import XmlRaport
-from my_tool import write
+import json
 
 info = "[*]"
 
 class SystemInfo(object):
 
-    os_info_s = []
+    os_info_dict: dict = {}
+    os_info_list: list = []
 
     def os_info(self):
 
@@ -34,19 +33,36 @@ class SystemInfo(object):
                              ("Architecture", Architecture),
                              ("Default_shell", default_shell),
                              ("Kernel", kernel_y)]
-            self.os_info_s = os_info
-     
+
+            self.os_info_dict['os_info'] = os_info
+            self.os_info_list = os_info
+
         except subprocess.TimeoutExpired as timeExpired:
             print(timeExpired)
             return None
 
+    def get_osInfo_dict(self):
+        return self.os_info_dict
+
+    def get_osInfo_list(self):
+        return self.os_info_list
+
+    def get_osInfo_list_json(self):
+        return json.dumps(self.get_osInfo_dict())
+
     def __str__(self):
-        return "Os_info"
+        return "System info"
 
 
 class ServiceInfo(object):
-    active_services = []
+    """ {'service_name':[('service_name', state)]}
+                        or
+            [('service_name', state)] """
+
+    active_services_list = []
+    active_services_dict = {}
     time = 0.0
+
     def service_info(self):
 
         start = time.time()
@@ -54,20 +70,35 @@ class ServiceInfo(object):
                                  shell=True,
                                  capture_output=True).stdout.decode("utf-8").split("\n")
 
-        # APPEND ACTIVE SERVICE TO self.active_services
-        tmpArr =[]
+        # APPEND ACTIVE SERVICE TO tmpArr: list
+        tmpArr = []
         for i in range(0, len(service)):
             if service[i] != "":
-                tmpArr.append(service[i].split(" ")[5])
+                tmpArr.append((service[i].split(" ")[5], "up"))
 
         end = time.time()
 
         self.time = end-start
-        self.active_services
+
+        self.active_services_list = tmpArr
+        self.active_services_dict['service_info'] = tmpArr
+        print(self.active_services_dict)
 
     def get_execution_time(self):
+        """ :return time execution"""
         return self.time
+
+    def get_active_service_list(self):
+        """ :return all active service in system as list"""
+        return self.active_services_list
+
+    def get_active_service_dict(self):
+        """ :return all active service in system as list as dictionary"""
+        return self.active_services_dict
+
+    def get_active_service_list_json(self):
+        """ :return all active service in system as json"""
+        return json.dumps(self.active_services_dict)
 
     def __str__(self):
         return "Service_Info"
-
